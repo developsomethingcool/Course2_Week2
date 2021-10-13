e4a608365cc4173ae8c8c3448326bbe84d0c3fa2
developsomethingcool <pometunalexander@gmail.com>
2021-10-13 15:37:54 +0200

    Added for loop

--- Final.R
+++ Final.R
@@ -1,9 +1,15 @@
 print("Hello World!")
 
-pollutantmean <- function(directory, pollutant, id){
-  fileref <- file(directory, "r")
-  data_base <- read.csv(fileref)
-  colMeans(data_base[pollutant], na.rm = TRUE, dims = 1)        
-}
+#pollutantmean <- function(directory, pollutant, id){
+ # fileref <- file(directory, "r")
+ # data_base <- read.csv(fileref)
+ # colMeans(data_base[pollutant], na.rm = TRUE, dims = 1)        
+#}
 
+fileslist <- list.files("specdata", full.names = TRUE)
+directory <- c(1)
+pollutant <- c(1)
+id <- c(1)
 
+df <- data.frame(directory, pollutant, id)
+print(df)
